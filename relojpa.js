5a44317d734e0f54f02ce4878e9b85c6a65d0d55
nikkkaido <catalina.gomez@mail.udp.cl>
2023-04-01 14:12:35 -0300

    reloj api

---
+++ relojpa.js
@@ -0,0 +1,21 @@
+function mostrarHora() {
+    const resultado = document.getElementById('resultado');
+    
+    fetch('http://worldtimeapi.org/api/timezone/America/Punta_Arenas')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.datetime);
+      //dividir datatime en fecha y hora
+      var time = data.datetime.split('T');
+      time = time[1].split('.');
+      time = time[0].split(':');
+      resultado.innerHTML = `
+        ${time[0]}:${time[1]}
+      `;
+    })
+  
+    .catch(error => console.error(error));
+    setTimeout(mostrarHora, 1000);
+  }
+  window.onload = mostrarHora;
+  
---
+++ relojstgo.js
@@ -0,0 +1,20 @@
+function mostrarHora() {
+  const resultado = document.getElementById('resultado');
+  
+  fetch('http://worldtimeapi.org/api/timezone/America/Santiago')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.datetime);
+    //dividir datatime en fecha y hora
+    var time = data.datetime.split('T');
+    time = time[1].split('.');
+    time = time[0].split(':');
+    resultado.innerHTML = `
+      ${time[0]}:${time[1]}
+    `;
+  })
+
+  .catch(error => console.error(error));
+  setTimeout(mostrarHora, 1000);
+}
+window.onload = mostrarHora;
